15e023971a6fab0fabe0ba0d63a0ec2de99dafc2
Robert joseph <robujsph2001@gmail.com>
2026-09-25 16:05:35 +0000

    Pin updated FloatLib and use its explicit rounding API

--- NN/MLTheory/CROWN/Extras/BoundOpsIEEE32Exec.lean
+++ NN/MLTheory/CROWN/Extras/BoundOpsIEEE32Exec.lean
@@ -30,12 +30,12 @@ namespace NN.MLTheory.CROWN
 /-- `BoundOps` for `ExecFloat.Binary 8 23`, using the executable directed-rounding endpoint
 primitives. -/
 instance (priority := 1000) : BoundOps (ExecFloat.Binary 8 23) where
-  addDown := (ExecFloat.Binary.add (rounding := .towardNegativeInfinity))
-  addUp   := (ExecFloat.Binary.add (rounding := .towardPositiveInfinity))
-  subDown := (ExecFloat.Binary.sub (rounding := .towardNegativeInfinity))
-  subUp   := (ExecFloat.Binary.sub (rounding := .towardPositiveInfinity))
-  mulDown := (ExecFloat.Binary.mul (rounding := .towardNegativeInfinity))
-  mulUp   := (ExecFloat.Binary.mul (rounding := .towardPositiveInfinity))
+  addDown := (ExecFloat.Binary.addWithRounding (rounding := .towardNegativeInfinity))
+  addUp   := (ExecFloat.Binary.addWithRounding (rounding := .towardPositiveInfinity))
+  subDown := (ExecFloat.Binary.subWithRounding (rounding := .towardNegativeInfinity))
+  subUp   := (ExecFloat.Binary.subWithRounding (rounding := .towardPositiveInfinity))
+  mulDown := (ExecFloat.Binary.mulWithRounding (rounding := .towardNegativeInfinity))
+  mulUp   := (ExecFloat.Binary.mulWithRounding (rounding := .towardPositiveInfinity))
 
 /--
 Nonlinear enclosures backed by the proved directed binary32 division and square-root operations.
@@ -46,14 +46,14 @@ enclosure of real exponentiation, so exponential and logarithmic transfers are i
 instance (priority := 1000) : NonlinearBoundOps (ExecFloat.Binary 8 23) where
   divBounds aLo aHi bLo bHi :=
     if NonlinearBoundOps.denominatorAvoidsZero bLo bHi then
-      let d1 := (ExecFloat.Binary.div (rounding := .towardNegativeInfinity)) aLo bLo
-      let d2 := (ExecFloat.Binary.div (rounding := .towardNegativeInfinity)) aLo bHi
-      let d3 := (ExecFloat.Binary.div (rounding := .towardNegativeInfinity)) aHi bLo
-      let d4 := (ExecFloat.Binary.div (rounding := .towardNegativeInfinity)) aHi bHi
-      let u1 := (ExecFloat.Binary.div (rounding := .towardPositiveInfinity)) aLo bLo
-      let u2 := (ExecFloat.Binary.div (rounding := .towardPositiveInfinity)) aLo bHi
-      let u3 := (ExecFloat.Binary.div (rounding := .towardPositiveInfinity)) aHi bLo
-      let u4 := (ExecFloat.Binary.div (rounding := .towardPositiveInfinity)) aHi bHi
+      let d1 := (ExecFloat.Binary.divWithRounding (rounding := .towardNegativeInfinity)) aLo bLo
+      let d2 := (ExecFloat.Binary.divWithRounding (rounding := .towardNegativeInfinity)) aLo bHi
+      let d3 := (ExecFloat.Binary.divWithRounding (rounding := .towardNegativeInfinity)) aHi bLo
+      let d4 := (ExecFloat.Binary.divWithRounding (rounding := .towardNegativeInfinity)) aHi bHi
+      let u1 := (ExecFloat.Binary.divWithRounding (rounding := .towardPositiveInfinity)) aLo bLo
+      let u2 := (ExecFloat.Binary.divWithRounding (rounding := .towardPositiveInfinity)) aLo bHi
+      let u3 := (ExecFloat.Binary.divWithRounding (rounding := .towardPositiveInfinity)) aHi bLo
+      let u4 := (ExecFloat.Binary.divWithRounding (rounding := .towardPositiveInfinity)) aHi bHi
       some (NonlinearBoundOps.min4 d1 d2 d3 d4, NonlinearBoundOps.max4 u1 u2 u3 u4)
     else
       none
@@ -64,8 +64,8 @@ instance (priority := 1000) : NonlinearBoundOps (ExecFloat.Binary 8 23) where
       none
     else
       let lo' := if lo > 0 then lo else 0
-      some ((ExecFloat.Binary.sqrt (rounding := .towardNegativeInfinity)) lo',
-        (ExecFloat.Binary.sqrt (rounding := .towardPositiveInfinity)) hi)
+      some ((ExecFloat.Binary.sqrtWithRounding (rounding := .towardNegativeInfinity)) lo',
+        (ExecFloat.Binary.sqrtWithRounding (rounding := .towardPositiveInfinity)) hi)
   sigmoidBounds := fun _ _ => some (0, 1)
   tanhBounds := fun _ _ => some ((-1), 1)
   sinBounds := fun _ _ => some ((-1), 1)
